ea4efdc64a0c9b32a6d2e1f60ca7c785b782aec0
Michael Joyner <joyner.michael@gmail.com>
2016-08-05 11:25:15 +0800

    checkout copy

--- resources/views/front/pages/checkout.blade.php
+++ resources/views/front/pages/checkout.blade.php
@@ -5,7 +5,7 @@
     <section class="checkout-container">
         <section class="checkout-help">
             <h2 class="text-green-hard sub-heading">What Happens Now?</h2>
-            <p>Once you send us this form you can sit back and let us take over. We will immediately process your order and get back to you with a quote and to organise your delivery. We take care of everything to make sure everything goes as smoothly as possible for you.</p>
+            <p>Once you send us this form you can sit back and let us take over. We will immediately process your order and get back to you with a quote and further instructions on how to finalise your purchase. We ship throughout Southern Africa.</p>
         </section>
         <section class="checkout-main">
             <section class="current-order">

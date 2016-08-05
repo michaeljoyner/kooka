8112652c710e8a798eb18a7057e30f04ed00e3b1
Michael Joyner <joyner.michael@gmail.com>
2016-08-05 11:19:06 +0800

    cart copy, css

--- resources/views/front/pages/cart.blade.php
+++ resources/views/front/pages/cart.blade.php
@@ -11,7 +11,7 @@
     </section>
     <section class="cart-help">
 
-        <p><span class="text-green-hard sub-heading">Note: </span>By proceeding to and completing the checkout process you are not committing to anything. You are just letting us know what you are looking for. Once you have submitted your quote request, we will take over and contact you with a quote and organise delivery, etc.</p>
+        <p><span class="text-green-hard sub-heading">Note: </span>By proceeding to and completing the checkout process you are not committing to anything. You are just letting us know what you are looking for. Once you have submitted your quote request, we will contact you with a quote and further instructions.</p>
     </section>
 
 @endsection

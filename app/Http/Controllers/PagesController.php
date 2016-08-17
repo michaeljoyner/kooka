8765adf42d47c3624b8fcd439a98cc7e701e61c1
Michael Joyner <joyner.michael@gmail.com>
2016-08-17 10:10:38 +0800

    get nine promoted products for carousel or fill with non promoted

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,7 +14,7 @@ class PagesController extends Controller
     public function home()
     {
         $categories = Category::all();
-        $topSellers = Product::all();
+        $topSellers = $this->getPromotedProducts();
         return view('front.pages.home')->with(compact('categories', 'topSellers'));
     }
 
@@ -42,5 +42,18 @@ class PagesController extends Controller
         $orderedItems = $cart->items()->values();
         $cart_subtotal = $cart->totalPrice();
         return view('front.pages.checkout')->with(compact('orderedItems', 'cart_subtotal'));
+    }
+
+    private function getPromotedProducts()
+    {
+        $promoted = Product::where('available', 1)->where('promoted', 1)->limit(9)->get();
+
+        if($promoted->count() < 9) {
+            $remaining = 9 - $promoted->count();
+            $filler = Product::where('available', 1)->where('promoted', 0)->orderByRaw('RAND()')->take($remaining)->get();
+            $promoted = $promoted->merge($filler);
+        }
+
+        return $promoted;
     }
 }
